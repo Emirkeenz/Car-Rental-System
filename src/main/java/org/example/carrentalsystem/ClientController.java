0e70e97f3f3246beb4e2658fda719b613c6c280d
Emirkeenz <orozbekovjob@gmail.com>
2024-12-01 00:40:59 +0600

    added fxml files and controllers for them, initialized 23 version maven

--- src/main/java/org/example/carrentalsystem/ClientController.java
+++ src/main/java/org/example/carrentalsystem/ClientController.java
@@ -3,7 +3,7 @@ package org.example.carrentalsystem;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ClientController {
     @FXML
     private Label welcomeText;
 
